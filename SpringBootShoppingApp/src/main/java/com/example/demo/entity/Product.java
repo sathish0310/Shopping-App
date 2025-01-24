f197b20a72bbb048d96c211f347981d7b65e742d
sathish <ssathisha470@gmail.com>
2025-01-24 16:48:19 +0530

    Added exception handler

--- SpringBootShoppingApp/src/main/java/com/example/demo/entity/Product.java
+++ SpringBootShoppingApp/src/main/java/com/example/demo/entity/Product.java
@@ -5,6 +5,10 @@ import java.util.Date;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.Data;
 
 @Entity
@@ -13,10 +17,20 @@ import lombok.Data;
 public class Product {
 	
 	@Id
+	@Min(value = 1, message = "Product ID must be greater than 0")
 	private int productId;
+
+	@NotBlank(message = "Product name is mandatory")
 	private String productName;
+
+	@Min(value = 0, message = "Product price must be non-negative")
 	private int productPrice;
+
+	@NotBlank(message = "Product category is mandatory")
 	private String productCategory;
+
+	@NotNull(message = "Product validity date is mandatory")
+	@PastOrPresent(message = "Product validity date must be in the past or present")
 	private Date productValidity;
 
 }
